2bd5e35f24e3cb3f756b4891f26b442d51bdcd95
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-09-17 08:10:42 -0400

    Add a keyboard layout switcher

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -101,6 +101,26 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
 
+
+-- Keyboard map indicator and changer
+kbdcfg = {}
+kbdcfg.cmd = "setxkbmap"
+kbdcfg.layout = { "us", "us -variant colemak" }
+kbdcfg.current = 1  -- us is our default layout
+kbdcfg.widget = widget({ type = "textbox", align = "right" })
+kbdcfg.widget.text = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+kbdcfg.switch = function ()
+   kbdcfg.current = kbdcfg.current % #(kbdcfg.layout) + 1
+   local t = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+   kbdcfg.widget.text = t
+   os.execute( kbdcfg.cmd .. t )
+end
+
+-- Mouse bindings
+kbdcfg.widget:buttons(awful.util.table.join(
+    awful.button({ }, 1, function () kbdcfg.switch() end)
+))
+
 -- Create a systray
 mysystray = widget({ type = "systray" })
 
@@ -179,6 +199,7 @@ for s = 1, screen.count() do
             layout = awful.widget.layout.horizontal.leftright
         },
         mylayoutbox[s],
+	kbdcfg.widget,
         mytextclock,
         s == 1 and mysystray or nil,
         mytasklist[s],
